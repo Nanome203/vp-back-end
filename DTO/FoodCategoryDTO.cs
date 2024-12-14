a951517a69810188e1995093b07794ee5781c279
Pham Minh Triet <vodanhtinh203@gmail.com>
2024-12-14 14:42:22 +0700

    add food category get api

---
+++ DTO/FoodCategoryDTO.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace vp_back_end.DTO;
+
+public class FoodCategoryDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "Chưa đặt tên";
+}
--- Services/FoodCategoryService.cs
+++ Services/FoodCategoryService.cs
@@ -1,8 +1,38 @@
 using System;
+using vp_back_end.DAO;
+using vp_back_end.DTO;
+using vp_back_end.Models;
 
 namespace vp_back_end.Services;
 
-public class FoodCategoryService
+public class FoodCategoryService(FoodCategoryDAO dao)
 {
+    private readonly FoodCategoryDAO foodCategoryDAO = dao;
 
+    public async Task<List<FoodCategoryDTO>> GetAllAsync()
+    {
+        try
+        {
+            var list = await foodCategoryDAO.GetAllAsync();
+            return ToFoodCategoryDTO(list);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return new List<FoodCategoryDTO>();
+        }
+    }
+
+    public List<FoodCategoryDTO> ToFoodCategoryDTO(List<FoodCategory> list)
+    {
+        List<FoodCategoryDTO> dtoList = new List<FoodCategoryDTO>();
+        foreach (var item in list)
+        {
+            FoodCategoryDTO dto = new FoodCategoryDTO();
+            dto.Id = item.Id;
+            dto.Name = item.Name;
+            dtoList.Add(dto);
+        }
+        return dtoList;
+    }
 }
