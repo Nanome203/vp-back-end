7e961a0665efbfe74f048ca68a86beeda48268fa
Pham Minh Triet <vodanhtinh203@gmail.com>
2024-12-14 15:13:15 +0700

    wrong file

--- Controllers/FoodCategoryController.cs
+++ Controllers/FoodCategoryController.cs
@@ -1,11 +1,22 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using vp_back_end.DTO;
+using vp_back_end.Services;
 
 namespace vp_back_end.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("food/categories")]
     [ApiController]
-    public class FoodController : ControllerBase
+    public class FoodCategoryController(FoodCategoryService service) : ControllerBase
     {
+        private readonly FoodCategoryService fcService = service;
+        [HttpGet]
+        public async Task<ActionResult<List<FoodCategoryDTO>>> GetAllAsync()
+        {
+            var list = await fcService.GetAllAsync();
+            if (list.Count == 0)
+                return NotFound();
+            return Ok(list);
+        }
     }
 }
---
+++ Controllers/FoodController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace vp_back_end.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodController : ControllerBase
+    {
+    }
+}
